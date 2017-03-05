253ad719b570b24235e8a517b88ae4267305fad8
marhoily <marhoily@homtail.com>
2017-03-05 15:55:37 +0300

    ExpressionsTest

--- Npc/PropertyObserver.cs
+++ Npc/PropertyObserver.cs
@@ -58,4 +58,89 @@ namespace Npc
                 _source.PropertyChanged -= OnPropertyChanged;
         }
     }
+    public interface ILink
+    {
+        object Value { get; }
+        void Subscribe(Action<object> changed);
+        void ChangeSource(object value);
+    }
+
+    public sealed class ConstLink : ResourceContainer, ILink
+    {
+        public string Name { get; }
+        private readonly Func<object, object> _exp;
+        private readonly List<Action<object>> _changed = new List<Action<object>>();
+        private object _source;
+        public object Value { get; private set; }
+        public void Subscribe(Action<object> handler) => _changed.Add(handler);
+
+        public ConstLink(string name, Func<object, object> exp)
+        {
+            Name = name;
+            _exp = exp;
+        }
+
+        public void ChangeSource(object source)
+        {
+            if (Equals(_source, source)) return;
+            _source = source;
+            var value = _exp(_source);
+            if (Equals(value, Value)) return;
+            Value = value;
+            _changed.ForEach(handle => handle(value));
+        }
+    }
+    public sealed class NpcLink : ResourceContainer, ILink
+    {
+        private INotifyPropertyChanged _source;
+        public readonly string PropertyName;
+        private readonly List<Action<object>> _changed = new List<Action<object>>();
+
+        public NpcLink(string propertyName)
+        {
+            PropertyName = propertyName;
+            Resources.Add(Unsubscribe);
+        }
+
+        public object Value { get; private set; }
+        public void Subscribe(Action<object> handler) => _changed.Add(handler);
+        public void ChangeSource(object source)
+        {
+            if (ReferenceEquals(_source, source))
+                return;
+            if (_source != null)
+            {
+                _source.PropertyChanged -= OnPropertyChanged;
+            }
+            _source = (INotifyPropertyChanged) source;
+            UpdateValue();
+            if (_source != null)
+            {
+                _source.PropertyChanged += OnPropertyChanged;
+            }
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == PropertyName)
+                UpdateValue();
+        }
+        private void UpdateValue()
+        {
+            var value = _source?
+                .GetType()
+                .GetProperty(PropertyName, Instance | Public | NonPublic)
+                .GetValue(_source);
+
+            if (Equals(Value, value))
+                return;
+            Value = value;
+            _changed.ForEach(handle => handle(value));
+        }
+        private void Unsubscribe()
+        {
+            if (_source != null)
+                _source.PropertyChanged -= OnPropertyChanged;
+        }
+    }
 }
