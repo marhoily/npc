bcb50342253c300ab4d1906cd91a6ae94395171a
marhoily <marhoily@homtail.com>
2017-03-05 18:29:37 +0300

    + Should_Unsubscribe

--- Npc.Tests/DependencyPropertyLinkTest.cs
+++ Npc.Tests/DependencyPropertyLinkTest.cs
@@ -6,11 +6,32 @@ namespace Npc.Tests
 {
     public sealed class DependencyPropertyLinkTest
     {
+        private readonly S[] _replacement = Chain(start: 'd', count: 3);
         private readonly S[] _original = Chain(start: 'a', count: 3);
         [Fact]
         public void Should_Extract_Value()
         {
-            _original[0].Track(x => x.D.Name).Value.Should().Be("b");
+            _original[0].Track(s => s.D.Name).Value.Should().Be("b");
+        }
+        [Fact]
+        public void Should_Follow_Change()
+        {
+            using (var valueObserver = _original[0].Track(s => s.D.Name))
+            {
+                _original[0].D = _replacement[0].X;
+                valueObserver.Value.Should().Be("e");
+            }
+        }
+        [Fact]
+        public void Should_Unsubscribe()
+        {
+            using (_original[0].Track(s => s.X.D.Name))
+            {
+                _original[1].ToString().Should().Be("bc*");
+                _original[0].X = _replacement[0].X;
+                _original[1].ToString().Should().Be("bc");
+            }
+            _replacement[0].ToString().Should().Be("def");
         }
     }
 }
---
+++ Npc/DependencyPropertyLink.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Npc
+{
+    public sealed class DependencyPropertyLink : ResourceContainer, ILink
+    {
+        private DependencyObject _source;
+        private readonly DependencyProperty _declaration;
+        private readonly DependencyPropertyDescriptor _descriptor;
+        private readonly List<Action<object>> _changed = new List<Action<object>>();
+
+        public DependencyPropertyLink(DependencyProperty declaration)
+        {
+            _declaration = declaration;
+            _descriptor = DependencyPropertyDescriptor.FromProperty(declaration, declaration.OwnerType);
+            Resources.Add(Unsubscribe);
+        }
+
+        public object Value { get; private set; }
+        public Type FormalType => _declaration.PropertyType;
+
+        public void Subscribe(Action<object> handler) => _changed.Add(handler);
+
+        public void ChangeSource(object value)
+        {
+            if (ReferenceEquals(_source, value))
+                return;
+            if (_source != null)
+            {
+                _descriptor.RemoveValueChanged(_source, OnPropertyChanged);
+            }
+            _source = (DependencyObject)value;
+            UpdateValue();
+            if (_source != null)
+            {
+                _descriptor.AddValueChanged(_source, OnPropertyChanged);
+            }
+        }
+        private void OnPropertyChanged(object sender, EventArgs eventArgs)
+        {
+            UpdateValue();
+        }
+        private void UpdateValue()
+        {
+            var value = _source.GetValue(_declaration);
+
+            if (Equals(Value, value))
+                return;
+            Value = value;
+            _changed.ForEach(handle => handle(value));
+        }
+        private void Unsubscribe()
+        {
+            if (_source != null)
+                _descriptor.RemoveValueChanged(_source, OnPropertyChanged);
+        }
+
+        public override string ToString() => $"DependencyProperty({_declaration.Name})";
+    }
+}
